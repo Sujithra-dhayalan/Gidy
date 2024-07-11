2cc43df03943f651d8700cb724f74e5693f61da0
Sujithra-dhayalan <sujithradhayalan@gmail.com>
2024-07-11 12:21:04 +0530

    js

---
+++ IntroductionToJS/Exercise1/one.js
@@ -0,0 +1,12 @@
+var firstName='Sujithra_d'
+var age=19
+var city='Chennai'
+var isStudent=1
+var score=200
+var greeting="Hello, World!"
+console.log(firstName)
+console.log(age)
+console.log(city)
+console.log(isStudent)
+console.log(score)
+console.log(greeting)

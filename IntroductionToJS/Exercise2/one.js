bdcd3aaae35c3c075665dcdcf0487599c463ac8a
Sujithra-dhayalan <sujithradhayalan@gmail.com>
2024-07-11 12:30:56 +0530

    js

---
+++ IntroductionToJS/Exercise2/one.js
@@ -0,0 +1,17 @@
+
+// without the usage of symbol $
+function greet(name){
+    console.log("Hello",(name)); 
+}
+
+greet('Sujithra')
+
+// using $ symbol to replace the desired name
+
+function greeting(name){
+    return `Hello, ${name}` ;
+}
+
+console.log(greeting("Sujithra"))
+
+// for using $ we should not use quotes instead we have to use this ` 
